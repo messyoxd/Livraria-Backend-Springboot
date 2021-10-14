594248dedc46e8eb483dc049db6f52948a7c0e52
Messyo <messyoroboto@gmail.com>
2021-10-14 10:33:06 -0300

    Added entities

---
+++ src/main/java/com/messyo/livraria/emprestimo/entity/Emprestimo.java
@@ -0,0 +1,51 @@
+package com.messyo.livraria.emprestimo.entity;
+
+import com.messyo.livraria.entity.Auditable;
+import com.messyo.livraria.livro.entity.Livro;
+import com.messyo.livraria.usuario.entity.Usuario;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "emprestimos"
+)
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Emprestimo extends Auditable {
+    @Id
+    @SequenceGenerator(name = "emprestimo_sequence", sequenceName = "emprestimo_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "emprestimo_sequence")
+    private Long emprestimoId;
+
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    @JoinColumn(
+            name = "usuario_id",
+            referencedColumnName = "usuarioId"
+    )
+    private Usuario usuarioEmprestimo;
+
+    @JoinColumn(
+            name = "livro_id",
+            referencedColumnName = "livroId"
+    )
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private Livro livroEmprestimo;
+
+    @Column(name = "previsao_devolucao", nullable = false)
+    private LocalDateTime previsaoDevolucao;
+
+    @Column(name = "data_devolucao")
+    private LocalDateTime dataDevolucao;
+
+    @Column(name = "status_emprestimo", nullable = false, columnDefinition = "TEXT")
+    private String statusEmprestimo;
+
+}
---
+++ src/main/java/com/messyo/livraria/livro/entity/Livro.java
@@ -0,0 +1,52 @@
+package com.messyo.livraria.livro.entity;
+
+import com.messyo.livraria.editora.entity.Editora;
+import com.messyo.livraria.entity.Auditable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "livros"
+)
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Livro extends Auditable {
+    @Id
+    @SequenceGenerator(name = "livro_sequence", sequenceName = "livro_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "livro_sequence")
+    private Long livroId;
+
+    @ManyToOne(
+            optional = false,
+            fetch = FetchType.EAGER
+    )
+    @JoinColumn(
+            name = "editora_id",
+            referencedColumnName = "editoraId"
+    )
+    private Editora editora;
+
+    @Column(name = "autor", nullable = false, columnDefinition = "TEXT")
+    private String autor;
+
+    @Column(name = "nome_livro", nullable = false, columnDefinition = "TEXT")
+    private String nomeLivro;
+
+    @Column(name = "lancamento", nullable = false)
+    private LocalDateTime lancamento;
+
+    @Column(name = "quantidade_disponivel", nullable = false)
+    private Integer quantidadeDisponivel;
+
+}
+
