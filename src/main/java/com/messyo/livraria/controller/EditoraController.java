78e8172c5f36384fc8dc06100e95168f833e9c76
Messyo <messyoroboto@gmail.com>
2021-10-14 08:20:59 -0300

    Add swagger

--- src/main/java/com/messyo/livraria/controller/EditoraController.java
+++ src/main/java/com/messyo/livraria/controller/EditoraController.java
@@ -1,5 +1,8 @@
 package com.messyo.livraria.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/editoras")
 public class EditoraController {
 
+    @ApiOperation(value="Returns a hello world")
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message = "Success method return")
+    })
     @GetMapping
     public String helloWorld(){
         return "Hello World";
