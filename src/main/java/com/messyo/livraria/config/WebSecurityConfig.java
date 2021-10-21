651b4d4d5d566f64f9511553163368e1cf49c6b5
Messyo <messyoroboto@gmail.com>
2021-10-21 11:16:28 -0300

    Refactored role permition for access usuarios api

--- src/main/java/com/messyo/livraria/config/WebSecurityConfig.java
+++ src/main/java/com/messyo/livraria/config/WebSecurityConfig.java
@@ -63,22 +63,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//                .authorizeRequests().antMatchers(USUARIOS_API_URL, H2_CONSOLE_URL, SWAGGER_URL).permitAll()
-//                .antMatchers(EDITORAS_API_URL, LIVROS_API_URL).hasAnyRole(ROLE_ADMIN)
-//                .antMatchers(EMPRESTIMOS_API_URL).hasAnyRole(ROLE_ADMIN, ROLE_USER)
-//                .anyRequest().authenticated()
-//                .and()
-//                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
-//                .and().sessionManagement()
-//                .sessionCreationPolicy(STATELESS);
-//
-//        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-//
-//        http.headers().frameOptions().disable();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers(USUARIOS_API_URL, H2_CONSOLE_URL, SWAGGER_URL).permitAll();
+        http.authorizeRequests().antMatchers(H2_CONSOLE_URL, SWAGGER_URL).permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.POST, USUARIOS_API_REGISTER_URL, USUARIOS_API_LOGIN_URL).permitAll();
         http.authorizeRequests().antMatchers(EDITORAS_API_URL, LIVROS_API_URL).hasAnyRole(ROLE_ADMIN);
         http.authorizeRequests().antMatchers(EMPRESTIMOS_API_URL).hasAnyRole(ROLE_ADMIN, ROLE_USER);
